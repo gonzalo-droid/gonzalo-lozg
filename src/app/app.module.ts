93dae385d7b9409d0484bdd8c2a7ab50235cd045
gonzalo.lozg <gonzalo.lozg@gmail.com>
2022-09-24 01:31:19 -0500

    fisrt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+
+import { NgxBootstrapIconsModule, allIcons } from 'ngx-bootstrap-icons';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeroComponent } from './components/hero/hero.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProjectComponent } from './components/project/project.component';
+import { AboutComponent } from './components/about/about.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HomeComponent,
+    HeaderComponent,
+    HeroComponent,
+    ContactComponent,
+    ProjectComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxBootstrapIconsModule.pick(allIcons),
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
