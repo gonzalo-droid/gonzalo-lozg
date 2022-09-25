f7d2911dc19210559894e210111127c9d22e8cf2
gonzalo.lozg <gonzalo.lozg@gmail.com>
2022-09-25 15:36:15 -0500

    Rename path from portfolio to gonzalo-lozg

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'portfolio'`, () => {
+  it(`should have as title 'gonzalo-lozg'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('portfolio');
+    expect(app.title).toEqual('gonzalo-lozg');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('portfolio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('gonzalo-lozg app is running!');
   });
 });
